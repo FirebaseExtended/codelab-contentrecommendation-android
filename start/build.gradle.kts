a0529840a748c969d11c116d80775bdec7343490
Marina Coelho <marinacoelho16@gmail.com>
2023-05-12 11:56:04 +0100

    Migrated gradle files in start folder

--- start/build.gradle.kts
+++ start/build.gradle.kts
@@ -19,30 +19,24 @@
  */
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    ext.kotlin_version = "1.6.20"
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:7.1.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath 'com.google.gms:google-services:4.3.10'
-        classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2"
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("androidx.navigation.safeargs") version "2.5.3" apply false
 }
 
 allprojects {
     repositories {
         google()
+        mavenLocal()
         mavenCentral()
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
 }
